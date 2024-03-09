99adbaedcfe2d06327c8a688ea0322d201a370b8
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-08 22:18:19 -0500

    saves blogs to local storage

---
+++ assets/js/form.js
@@ -0,0 +1,68 @@
+const form = document.querySelector('#form');
+const userName = document.querySelector('#name');
+const title = document.querySelector('#title');
+const content = document.querySelector('#content');
+
+const nameError = document.querySelector('#name-error');
+const titleError = document.querySelector('#title-error');
+const contentError = document.querySelector('#content-error');
+
+let validated = false;
+const formInputs = [
+  { input: userName, error: nameError, display: 'name' },
+  { input: title, error: titleError, display: 'title' },
+  { input: content, error: contentError, display: 'content' },
+];
+
+let blogs = [];
+
+function init() {
+  //   userName.addEventListener('keyup', () => validateEntry(userName, nameError));
+  formInputs.forEach(({ input, error, display }) => {
+    input.addEventListener('keyup', () => validateEntry(input, error, display));
+  });
+
+  const existingBlogs = localStorage.getItem('blogs');
+  if (existingBlogs) {
+    blogs = JSON.parse(existingBlogs);
+  }
+}
+
+function saveLocalStorage(data) {
+  localStorage.setItem('blogs', JSON.stringify(data));
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  if (!validated) {
+    return;
+  }
+  const newBlog = {
+    userName: userName.value,
+    title: title.value,
+    content: content.value,
+  };
+  blogs.push(newBlog);
+  saveLocalStorage(blogs);
+}
+
+function validateEntry(input, error, display) {
+  validated = true;
+  if (display === 'name' && /\d/.test(input.value)) {
+    error.textContent = "You're name can't include a number";
+    validated = false;
+  }
+
+  if (input.value.length < 2) {
+    error.textContent = `You're ${display} has to be longer than 2 characters`;
+    validated = false;
+  }
+
+  if (validated) {
+    error.textContent = '';
+  }
+}
+
+form.addEventListener('submit', submitForm);
+
+init();
