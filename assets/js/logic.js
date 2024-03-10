4117d3f16d28a9481346a07e812f5f2bf25635d2
Hugo Hsi <hhsi@hugodehsign.com>
2024-03-10 03:11:46 -0400

    added comments to files, describes functionality of code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,12 @@
+// get references to body and theme toggle button
 const docBody = document.querySelector('body');
 const themeToggle = document.querySelector('.theme-button');
+
+// initialize global theme variable
 let theme;
 
+// initializes theme to be light if none exists
+// sets to theme saved in local storage
 function initTheme() {
   theme = localStorage.getItem('theme');
   if (!theme) {
@@ -11,16 +16,22 @@ function initTheme() {
   docBody.dataset['theme'] = theme;
 }
 
+// function for toggling theme
 function toggleTheme() {
+  // if statement toggles them between light and dark
   if (theme === 'light') {
     theme = 'dark';
   } else {
     theme = 'light';
   }
+  // sets body data to selected theme
   docBody.dataset['theme'] = theme;
+  // saves theme to local storage
   localStorage.setItem('theme', theme);
 }
 
+// function initializes theme
 initTheme();
 
+// toggles theme on button click
 themeToggle.addEventListener('click', toggleTheme);
